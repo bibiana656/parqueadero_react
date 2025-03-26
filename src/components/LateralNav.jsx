b45fc305755be5500b9578198c826a9928e23c64
josanva777 <johansanchezvalencia@gmail.com>
2025-03-26 15:56:37 -0500

    mejorando el formulario, el estado del lateralvar para saber si estoy ubicado en crear cuenta, se elimine de la vista

--- src/components/LateralNav.jsx
+++ src/components/LateralNav.jsx
@@ -1,63 +1,68 @@
 import "./LateralNav.css";
 import { FaRegCalendar } from "react-icons/fa";
 import '../../src/index.css';
-
-
+import { Link, useLocation } from 'react-router-dom'; 
 
 function LateralNav() {
-    return (
-        <aside className="sidebar">
-          <div className="sidebar-content">
-            <ul className="sidebar-menu">
-              <li>
-                <a href="#">
-                <FaRegCalendar className="icon" />
-                  <span>Disponibilidad</span>
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                  <span>Entrada</span>
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                  <span>Salida</span>
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                  <span>Mensualidad</span>
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                  <span>Reserva</span>
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                  <span>Reportes</span>
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                  <span>Ayuda</span>
-                </a>
-              </li>
-              <li>
-                <a href="#">
-                  <span>Politicas</span>
-                </a>
-              </li>
-            </ul>
-            <div className="sidebar-footer">
-              <button>
-                Crear cuenta
-              </button>
-            </div>
-          </div>
-        </aside>
-      );
-    };
+  const location = useLocation();
+  const isRegistroRoute = location.pathname === '/formularioregistro'; 
+
+  return (
+    <aside className="sidebar">
+      <div className="sidebar-content">
+        <ul className="sidebar-menu">
+          <li>
+            <a href="#">
+              <FaRegCalendar className="icon" />
+              <span>Disponibilidad</span>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <span>Entrada</span>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <span>Salida</span>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <span>Mensualidad</span>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <span>Reserva</span>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <span>Reportes</span>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <span>Ayuda</span>
+            </a>
+          </li>
+          <li>
+            <a href="#">
+              <span>Politicas</span>
+            </a>
+          </li>
+        </ul>
+        <div className="sidebar-footer">
+          {!isRegistroRoute && (
+            <Link to="/formularioregistro">
+              Crear cuenta
+            </Link>
+          )}
+        </div>
+      </div>
+    </aside>
+  );
+}
+
 export default LateralNav;
